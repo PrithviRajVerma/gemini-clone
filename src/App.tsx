cd12c2c69060488752fec2d8748a938b14632448
TheEmperorPR <2266prr@gmail.com>
2025-10-06 23:22:36 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Sidebar from './components/Sidebar/Sidebar.tsx';
+import Main from './components/Main/Main.tsx';
+
+const App = () => {
+
+
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App;
